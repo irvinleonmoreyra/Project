04104b36f67803a8f3261016c9bec2ca95b563e6
Irvin <irvin.leon@gmail.com>
2022-04-09 13:08:12 -0500

    first modification in client.py and new file numbers.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from operation_package import operation
 def main():
-    result = operation.area_of_circle(2)
+    result = operation.area_of_circle(3)
     print("The area of the circle is: {}".format(result))
 
 if __name__ == '__main__':
---
+++ numbers.py
@@ -0,0 +1,4 @@
+age = input("insert your age: ")
+print(type(float(age)))
+new_age=int(age) +5
+print(new_age)
