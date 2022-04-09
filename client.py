e95284c68e69d3ced83d437520f573bdfaed429f
irvinleonmoreyra <102266839+irvinleonmoreyra@users.noreply.github.com>
2022-04-09 12:50:21 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,8 @@
+from operation_package import operation
+def main():
+    result = operation.area_of_circle(2)
+    print("The area of the circle is: {}".format(result))
+
+if __name__ == '__main__':
+    main()
+
---
+++ iterator_call.py
@@ -0,0 +1,15 @@
+from iterator_class import SequenceNumber
+
+def main():
+    sequence_number=SequenceNumber(0,3)
+    #print 0
+    print(next(sequence_number))
+    #print 1
+    print(next(sequence_number))
+    #print 2
+    print(next(sequence_number))
+    #and error ocurred: Stopiteration
+    print(next(sequence_number))
+
+if __name__=='__main__':
+    main()
